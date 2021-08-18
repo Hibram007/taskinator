f52608540072148ae773b8a22f82e4afd1c6fdcb
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-08-17 21:37:48 -0700

    added submit event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-var buttonEl = document.querySelector("#save-task"); 
+var formEl = document.querySelector("#task-form"); 
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-//This is defining the function (intsructions)
-var createTaskHandler = function() { 
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
+
   var listItemEl = document.createElement("li"); 
   listItemEl.className = "task-item"; 
   listItemEl.textContent = "This is a new task."; 
   tasksToDoEl.appendChild(listItemEl); 
   }; 
 
-  //This is calling the function (activating)
-buttonEl.addEventListener("click", createTaskHandler);
+  formEl.addEventListener("submit", createTaskHandler);
